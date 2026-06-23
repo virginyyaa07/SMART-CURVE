24250b5e7e21c529389c9c6970561143c4b95cb5
virginyyaa07 <virginyyaa07@gmail.com>
2026-06-23 15:54:40 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function simulasi() {
+
+    const status = document.getElementById("status");
+
+    status.innerHTML = `
+        <h2>🔴 PERINGATAN AKTIF</h2>
+        <p>🚦 Lampu Peringatan Menyala</p>
+        <p>🔊 Buzzer Aktif</p>
+        <p>📡 Kendaraan Terdeteksi</p>
+    `;
+}
